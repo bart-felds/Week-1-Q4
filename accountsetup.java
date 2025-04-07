273360629f7eb2251af9755946471cfc68e9b113
bart-felds <bart.fieldsiii@gmail.com>
2025-04-07 10:26:41 -0400

    oij
    oij

--- accountsetup.java
+++ accountsetup.java
@@ -6,11 +6,20 @@ import java.util.*;
 public class accountsetup {
     public static void main(String[]args){
         Scanner scan = new Scanner(System.in);
-        boolean Created;
-        Created = true;
-        while (Created == false){
-            System.out.println("False");
+        
+        System.out.println("Enter a username");
+        String Username = scan.next();
+        System.out.println("Enter a password that is at least 8 characters");
+        String Password = scan.next();
+        
+            while (Password.length()<8){
+            System.out.println("Your password is less than 8, make a new one");
+            Password = scan.next();
         }
+
+
+
+        System.out.println("Your username is " + Username.toLowerCase()+ " and your password is "+Password.toLowerCase());
 
     }
 }
---
+++ stringshort.java
@@ -0,0 +1,9 @@
+public class stringshort {
+    public static void main(String[]args){
+        String x = "lots of words.";
+        String y = "Lots of Words.";
+        String z = "Two words.";
+        System.out.println(x.compareToIgnoreCase(z));
+    }
+
+}
