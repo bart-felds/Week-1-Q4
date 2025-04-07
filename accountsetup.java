d8d84e2a7e87c5ac6be04d40bc8a3f1f705e2eda
bart-felds <bart.fieldsiii@gmail.com>
2025-04-07 10:12:29 -0400

    Create accountsetup.java

---
+++ accountsetup.java
@@ -0,0 +1,16 @@
+//barrington fields
+//currently the date i 4/7/2025
+import java.util.*;
+
+
+public class accountsetup {
+    public static void main(String[]args){
+        Scanner scan = new Scanner(System.in);
+        boolean Created;
+        Created = true;
+        while (Created == false){
+            System.out.println("False");
+        }
+
+    }
+}
